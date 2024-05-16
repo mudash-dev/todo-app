4df141b8a63156636af64f548a03b834a2e4bf24
mudash-dev <platform.intern@dlight.com>
2024-05-16 16:37:13 +0300

    Create necessary components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export default function TodoItem(
+    { task, deleteTask, toggleCompleted }
+) {
+    function handleChange() {
+        toggleCompleted(task.id);
+    }
+
+    return (
+        <section>
+            <div className="todo-item">
+                <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={handleChange}
+                />
+                <p>{task.text}</p>
+                <button onClick={() => deleteTask(task.id)}>
+                    X
+                </button>
+            </div>
+        </section>
+    );
+
+}
